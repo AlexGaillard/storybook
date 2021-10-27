02f4d6fa0ee3b7dac890931644b8ac10beb029ce
AlexGaillard <alex@radish.health>
2021-10-27 14:21:15 -0400

    Added published storybook for example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button} from './stories/Button.jsx'
+import {Title} from './stories/Title.jsx'
+
 
 function App() {
   return (
     <div className="App">
-      <Button size="large" label="test"></Button>
+      <Title primary label="Hello World" />
+      <Button large primary size="large" label="CLICK ME"></Button>
     </div>
   );
 }
